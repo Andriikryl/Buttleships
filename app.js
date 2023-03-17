227ed4cad4ffebe1efc7c473b1bcadb9165852c6
Amdrii <andreyk557@gmail.com>
2023-03-17 18:40:03 +0200

    feat[cl/ship]

--- app.js
+++ app.js
@@ -12,16 +12,42 @@ function flip() {
   );
 }
 
+flipButton.addEventListener("click", flip);
+
 const width = 10;
 
-function createBoard(color) {
+function createBoard(color, user) {
   const gameBordContainer = document.createElement("div");
   gameBordContainer.classList.add("game-board");
   gameBordContainer.style.backgroundColor = color;
+  gameBordContainer.id = user;
+
+  for (let i = 0; i < width * width; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.id = i;
+    gameBordContainer.append(block);
+  }
+
   gamesBordContainer.append(gameBordContainer);
 }
 
-createBoard("#0077b6");
-createBoard("#00b4d8");
+createBoard("#0077b6", "player");
+createBoard("#00b4d8", "computer");
 
-flipButton.addEventListener("click", flip);
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+  }
+}
+
+const destroyer = new Ship("destroyer", 2);
+const submarine = new Ship("submarine", 3);
+const cruiser = new Ship("cruiser", 3);
+const battleship = new Ship("battleship", 4);
+const carrier = new Ship("carrier", 5);
+
+const ships = [destroyer, submarine, cruiser, battleship, carrier];
+
+function addShipPiece() {}
