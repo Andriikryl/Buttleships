aba7bf901bac88cbcf0980f16d9eb75de78243d7
Amdrii <andreyk557@gmail.com>
2023-03-17 18:25:03 +0200

    feat[createBoard]

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const optionContainer = document.querySelector(".option-containet");
 const flipButton = document.querySelector("#flip-button");
+const gamesBordContainer = document.querySelector("#gamesboards-container");
 
 let angel = 0;
 
@@ -10,5 +11,17 @@ function flip() {
     (optionShip) => (optionShip.style.transform = `rotate(${angel}deg)`)
   );
 }
+
+const width = 10;
+
+function createBoard(color) {
+  const gameBordContainer = document.createElement("div");
+  gameBordContainer.classList.add("game-board");
+  gameBordContainer.style.backgroundColor = color;
+  gamesBordContainer.append(gameBordContainer);
+}
+
+createBoard("#0077b6");
+createBoard("#00b4d8");
 
 flipButton.addEventListener("click", flip);
