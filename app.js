85b69d329afba7115e86e19890527ef18ca404b8
Amdrii <andreyk557@gmail.com>
2023-03-17 20:26:02 +0200

    feat[drug-drop]

--- app.js
+++ app.js
@@ -49,20 +49,23 @@ const battleship = new Ship("battleship", 4);
 const carrier = new Ship("carrier", 5);
 
 const ships = [destroyer, submarine, cruiser, battleship, carrier];
+let notDropped;
 
-function addShipPiece(ship) {
-  const allBoardBlocks = document.querySelectorAll("#computer div");
+function addShipPiece(user, ship, startId) {
+  const allBoardBlocks = document.querySelectorAll(`#${user} div`);
   let randomBoolean = Math.random() < 0.5;
-  let isHorizontal = randomBoolean;
+  let isHorizontal = user === "player" ? angel === 0 : randomBoolean;
   let randomStartIndex = Math.floor(Math.random() * width * width);
 
+  let startIndex = startId ? startId : randomStartIndex;
+
   let validStart = isHorizontal
-    ? randomStartIndex <= width * width - ship.length
-      ? randomStartIndex
+    ? startIndex <= width * width - ship.length
+      ? startIndex
       : width * width - ship.length
-    : randomStartIndex <= width * width - width * ship.length
-    ? randomStartIndex
-    : randomStartIndex - ship.length * width + width;
+    : startIndex <= width * width - width * ship.length
+    ? startIndex
+    : startIndex - ship.length * width + width;
 
   let shipBlocks = [];
 
@@ -100,8 +103,40 @@ function addShipPiece(ship) {
       shipBlock.classList.add("taken");
     });
   } else {
-    addShipPiece(ship);
+    if (user === "computer") addShipPiece(ship);
+    if (user === " player") notDropped = true;
   }
 }
 
-ships.forEach((ship) => addShipPiece(ship));
+ships.forEach((ship) => addShipPiece("computer", ship));
+
+let draggedShip;
+
+const optionShips = Array.from(optionContainer.children);
+optionShips.forEach((optionShip) =>
+  optionShip.addEventListener("dragstart", dragStart)
+);
+
+const allPlayerBlocks = document.querySelectorAll("#player div");
+allPlayerBlocks.forEach((playerBlock) => {
+  playerBlock.addEventListener("dragover", dragOver);
+  playerBlock.addEventListener("drop", dropShip);
+});
+
+function dragStart(e) {
+  notDropped = false;
+  draggedShip = e.tatget;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dropShip() {
+  const startId = e.tatget.id;
+  const ship = ships[draggedShip.id];
+  addShipPiece("player", ship, startId);
+  if (!notDropped) {
+    draggedShip.remove();
+  }
+}
